3c075681efa0c68e55fe2eea8e842546403930e5
Kivia Cristina Marqueto de Campos <137660962+kiviamarquetodecampos@users.noreply.github.com>
2023-09-15 15:22:44 -0300

    Update 1_visao_empresa.py
    Correção caminho .csv

--- pages/1_visao_empresa.py
+++ pages/1_visao_empresa.py
@@ -139,7 +139,7 @@ def country_maps(df1):
 #=====================================
 #Dataset
 #=====================================
-df = pd.read_csv("../train.csv")
+df = pd.read_csv("train.csv")
 
 #=====================================
 #Limpanda os dados
