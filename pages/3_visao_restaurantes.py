2be100063ed15f9c5ad37573dd57878eeb501f5b
Kivia Cristina Marqueto de Campos <137660962+kiviamarquetodecampos@users.noreply.github.com>
2023-09-15 16:18:05 -0300

    Update 3_visao_restaurantes.py
    correção data

--- pages/3_visao_restaurantes.py
+++ pages/3_visao_restaurantes.py
@@ -10,7 +10,7 @@ from PIL import Image
 import folium
 from streamlit_folium import folium_static
 import numpy as np
-
+from datetime import datetime
 
 st.set_page_config(page_title="Visão Restaurantes", page_icon="", layout="wide")
 
@@ -158,9 +158,9 @@ st.sidebar.markdown("## Selecione uma data limite")
 
 #Seleção data
 date_slider = st.sidebar.slider("Até qual valor?", 
-                  value = pd.datetime(2022, 4, 13),
-                  min_value = pd.datetime(2022, 2, 11),
-                  max_value = pd.datetime(2022, 4, 6),
+                  value = datetime(2022, 4, 13),
+                  min_value = datetime(2022, 2, 11),
+                  max_value = datetime(2022, 4, 6),
                  format = "DD-MM-YYYY")
 
 st.sidebar.markdown("""---""")
